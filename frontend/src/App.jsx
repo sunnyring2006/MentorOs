295d605fb6b09736e461ea8abc537c0733056652
student-walchandcollege <patilkimaya01@gmail.com>
2026-07-23 17:46:33 +0530

    Day 4: Added name input UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
 
 function App() {
+  const [name, setName] = useState("");
   return (
     <h1>Welcome to MentorOS 🚀</h1>
+    <input type="text"placeholder="Enter your name"/>
   );
 }
 
